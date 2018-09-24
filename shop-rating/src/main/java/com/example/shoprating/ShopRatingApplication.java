ebbe33812b37eb4993356895312d22e13556cf3c
dennis.susat <dennis.susat@marconirovereto.it>
2018-09-24 08:48:13 +0200

    punto iniziale

---
+++ shop-rating/src/main/java/com/example/shoprating/ShopRatingApplication.java
@@ -0,0 +1,12 @@
+package com.example.shoprating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopRatingApplication.class, args);
+	}
+}
